3dc7ed27633ba38a1852aab3f204c360743cec12
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-07-03 17:52:24 -0400

    code refactoring and writing test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,18 @@
 const request = require('request');
-const arr = process.argv.slice(2);
-let newSearch = 'https://api.thecatapi.com/v1/breeds/search?q=' + arr[0];
-request(newSearch, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  if (data.length !== 0) {
-    console.log(data[0]['description']);
+const fetchBreedDescription = function(breedName, callback) {
+  let newSearch = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(newSearch, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length !== 0) {
+        callback(null, data[0]['description']);
 
-  } else {
-    console.log("The requested breed is not found");
-  }
-});
+      } else {
+        callback("The requested breed is not found",null);
+      }
+    }
+  });
+};
+module.exports = { fetchBreedDescription }; 
